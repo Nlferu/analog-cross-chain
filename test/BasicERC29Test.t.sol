ac3f24046a1501faf16201196119026bc7ada437
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-10-18 17:56:26 +0200

    logs added to main test

--- test/BasicERC29Test.t.sol
+++ test/BasicERC29Test.t.sol
@@ -1,19 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.25;
 
-import {Test} from "forge-std/Test.sol";
+import {Test, console} from "forge-std/Test.sol";
 import {BasicERC20} from "../src/BasicERC20.sol";
 import {GmpTestTools} from "@analog-gmp-testing/GmpTestTools.sol";
 import {Gateway} from "@analog-gmp/Gateway.sol";
-import {IGateway} from "@analog-gmp/interfaces/IGateway.sol";
 import {GmpMessage, GmpStatus, GmpSender, PrimitiveUtils} from "@analog-gmp/Primitives.sol";
+
+import {IGateway} from "@analog-gmp/interfaces/IGateway.sol";
 
 contract BasicERC20Test is Test {
     using PrimitiveUtils for GmpSender;
     using PrimitiveUtils for address;
 
-    address private constant ALICE = address(bytes20(keccak256("Alice")));
-    address private constant BOB = address(bytes20(keccak256("Bob")));
+    address private ALICE = makeAddr("Alice");
+    address private BOB = makeAddr("Bob");
 
     Gateway private constant SEPOLIA_GATEWAY = Gateway(GmpTestTools.SEPOLIA_GATEWAY);
     uint16 private constant SEPOLIA_NETWORK = GmpTestTools.SEPOLIA_NETWORK_ID;
@@ -21,24 +22,23 @@ contract BasicERC20Test is Test {
     Gateway private constant SHIBUYA_GATEWAY = Gateway(GmpTestTools.SHIBUYA_GATEWAY);
     uint16 private constant SHIBUYA_NETWORK = GmpTestTools.SHIBUYA_NETWORK_ID;
 
-    // 14882540 14984307
     /// @dev Test the teleport of tokens from Alice's account in Shibuya to Bob's account in Sepolia
-    function test_teleportTokens() external {
-        ////////////////////////////////////
-        // Step 1: Setup test environment //
-        ////////////////////////////////////
+    function test_teleportTokens() public {
+        console.log("Alice Address: ", ALICE);
+        console.log("Bob Address: ", BOB);
 
-        // Deploy the gateway contracts at pre-defined addresses
-        // Also creates one fork for each supported network
         GmpTestTools.setup();
+
+        console.log("\n  Sepolia Gateway Contract: ", address(SEPOLIA_GATEWAY));
+        console.log("Shibuya Gateway Contract: ", address(SHIBUYA_GATEWAY));
 
         // Add funds to Alice and Bob in all networks
         GmpTestTools.deal(ALICE, 100 ether);
         GmpTestTools.deal(BOB, 100 ether);
 
-        ///////////////////////////////////////////////////////
-        // Step 2: Deploy the sender and recipient contracts //
-        ///////////////////////////////////////////////////////
+        ///////////////////////////////////////////////
+        // Deploy the sender and recipient contracts //
+        ///////////////////////////////////////////////
 
         // Pre-compute the contract addresses, because the contracts must know each other addresses.
         BasicERC20 shibuyaErc20 = BasicERC20(vm.computeCreateAddress(ALICE, vm.getNonce(ALICE)));
@@ -56,13 +56,21 @@ contract BasicERC20Test is Test {
         assertEq(sepoliaErc20.balanceOf(ALICE), 0, "unexpected alice balance in sepolia");
         assertEq(sepoliaErc20.balanceOf(BOB), 0, "unexpected bob balance in sepolia");
 
+        console.log("\nBalances before bridge...");
+        GmpTestTools.switchNetwork(SHIBUYA_NETWORK, ALICE);
+        console.log("SHIBUYA NETWORK -> Alice Token A Balance: ", shibuyaErc20.balanceOf(ALICE));
+        console.log("SHIBUYA NETWORK -> Bob Token A Balance: ", shibuyaErc20.balanceOf(BOB));
+        GmpTestTools.switchNetwork(SEPOLIA_NETWORK, ALICE);
+        console.log("SEPOLIA NETWORK -> Alice Token B Balance: ", sepoliaErc20.balanceOf(ALICE));
+        console.log("SEPOLIA NETWORK -> Bob Token B Balance: ", sepoliaErc20.balanceOf(BOB));
+
         // Check if the computed addresses matches
         assertEq(address(shibuyaErc20), vm.computeCreateAddress(ALICE, 0), "unexpected shibuyaErc20 address");
         assertEq(address(sepoliaErc20), vm.computeCreateAddress(BOB, 0), "unexpected sepoliaErc20 address");
 
-        //////////////////////////////
-        // Step 3: Send GMP message //
-        //////////////////////////////
+        //////////////////////
+        // Send GMP message //
+        //////////////////////
 
         // Switch to Shibuya network and Alice account
         GmpTestTools.switchNetwork(SHIBUYA_NETWORK, ALICE);
@@ -81,9 +89,9 @@ contract BasicERC20Test is Test {
         GmpTestTools.switchNetwork(SEPOLIA_NETWORK, ALICE);
         assertTrue(SEPOLIA_GATEWAY.gmpInfo(messageID).status == GmpStatus.NOT_FOUND, "unexpected message status, expect 'pending'");
 
-        ///////////////////////////////////////////////////
-        // Step 4: Wait Chronicles Relay the GMP message //
-        ///////////////////////////////////////////////////
+        ///////////////////////////////////////////
+        // Wait Chronicles Relay the GMP message //
+        ///////////////////////////////////////////
 
         // The GMP hasn't been executed yet...
         assertEq(sepoliaErc20.balanceOf(ALICE), 0, "unexpected alice balance in shibuya");
@@ -106,5 +114,13 @@ contract BasicERC20Test is Test {
         GmpTestTools.switchNetwork(SEPOLIA_NETWORK);
         assertEq(sepoliaErc20.balanceOf(ALICE), 0, "unexpected alice's balance in sepolia");
         assertEq(sepoliaErc20.balanceOf(BOB), 100, "unexpected bob's balance in sepolia");
+
+        console.log("\nBalances after bridge...");
+        GmpTestTools.switchNetwork(SHIBUYA_NETWORK, ALICE);
+        console.log("SHIBUYA NETWORK -> Alice Token A Balance: ", shibuyaErc20.balanceOf(ALICE));
+        console.log("SHIBUYA NETWORK -> Bob Token A Balance: ", shibuyaErc20.balanceOf(BOB));
+        GmpTestTools.switchNetwork(SEPOLIA_NETWORK, ALICE);
+        console.log("SEPOLIA NETWORK -> Alice Token B Balance: ", sepoliaErc20.balanceOf(ALICE));
+        console.log("SEPOLIA NETWORK -> Bob Token B Balance: ", sepoliaErc20.balanceOf(BOB));
     }
 }
