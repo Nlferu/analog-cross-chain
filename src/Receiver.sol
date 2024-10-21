99762800e3025d7673d8af4dc7e06ee28c1c5f38
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-10-22 01:22:53 +0200

    fresh receiver only contract

---
+++ src/Receiver.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25;
+
+// This will be our main contract with logic, which has onGMPReceive() with all possible payable functions (buy, buyout, refund etc.) triggers
+contract Receiver {
+
+}
