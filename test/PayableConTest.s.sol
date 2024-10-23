63605f59ec0ceeb8454866f05a65109b630dd088
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-10-24 01:56:30 +0200

    removed

--- test/PayableConTest.s.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.25;
-
-import {Test, console} from "forge-std/Test.sol";
-import {PayableCon} from "../src/PayableCon.sol";
-
-contract PayableConTest is Test {}
