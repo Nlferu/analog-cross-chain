50c58ee6408c723cd88543f519c2abdb14ef24d6
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-10-24 17:17:40 +0200

    client renamed

--- test/PrototypeTest.t.sol
+++ test/PrototypeTest.t.sol
@@ -16,7 +16,7 @@ contract PrototypeTest is Test {
 
     address private OWNER = makeAddr("Owner");
     address private DEVIL = makeAddr("Devil");
-    address private ALICE = makeAddr("Alice");
+    address private USER = makeAddr("User");
 
     Gateway private constant SEPOLIA_GATEWAY = Gateway(GmpTestTools.SEPOLIA_GATEWAY);
     uint16 private constant SEPOLIA_NETWORK = GmpTestTools.SEPOLIA_NETWORK_ID;
@@ -24,9 +24,9 @@ contract PrototypeTest is Test {
     Gateway private constant SHIBUYA_GATEWAY = Gateway(GmpTestTools.SHIBUYA_GATEWAY);
     uint16 private constant SHIBUYA_NETWORK = GmpTestTools.SHIBUYA_NETWORK_ID;
 
-    /// @dev Test the teleport of tokens from Alice's account in Shibuya to Bob's account in Sepolia
+    /// @dev Test the teleport of tokens from User's account in Shibuya to Bob's account in Sepolia
     function test_teleport() public {
-        console.log("Alice Address: ", ALICE);
+        console.log("User Address: ", USER);
         console.log("Devil Address: ", DEVIL);
 
         GmpTestTools.setup();
@@ -38,7 +38,7 @@ contract PrototypeTest is Test {
         /// @dev Test if normal .deal on 1 chain only will fail!!!
         GmpTestTools.deal(OWNER, 100 ether);
         GmpTestTools.deal(DEVIL, 100 ether);
-        GmpTestTools.deal(ALICE, 100 ether);
+        GmpTestTools.deal(USER, 100 ether);
 
         //////////////////////////////////////////////
         // Deploy the Sender and Receiver contracts //
@@ -49,7 +49,7 @@ contract PrototypeTest is Test {
         Sender sender = Sender(vm.computeCreateAddress(OWNER, vm.getNonce(OWNER)));
         Receiver receiver = Receiver(vm.computeCreateAddress(DEVIL, vm.getNonce(DEVIL)));
 
-        // Switch to Shibuya network and deploy the ERC20 using Alice account
+        // Switch to Shibuya network and deploy the ERC20 using User account
         GmpTestTools.switchNetwork(SEPOLIA_NETWORK, OWNER);
         USDT usdt = new USDT();
         sender = new Sender(SEPOLIA_GATEWAY, receiver, SHIBUYA_NETWORK, address(usdt));
@@ -61,20 +61,20 @@ contract PrototypeTest is Test {
         console.log("sender, receiver: ", address(sender), address(receiver));
 
         // Give user some USDT
-        GmpTestTools.switchNetwork(SEPOLIA_NETWORK, ALICE);
+        GmpTestTools.switchNetwork(SEPOLIA_NETWORK, USER);
         usdt.mint(1000);
 
         console.log("\nBalances before bridge...");
-        console.log("SEPOLIA NETWORK -> Alice USDT Balance: ", usdt.balanceOf(ALICE));
-        GmpTestTools.switchNetwork(SHIBUYA_NETWORK, ALICE);
-        console.log("SHIBUYA NETWORK -> Alice wUSDT Balance: ", wusdt.balanceOf(ALICE));
+        console.log("SEPOLIA NETWORK -> User USDT Balance: ", usdt.balanceOf(USER));
+        GmpTestTools.switchNetwork(SHIBUYA_NETWORK, USER);
+        console.log("SHIBUYA NETWORK -> User wUSDT Balance: ", wusdt.balanceOf(USER));
 
         //////////////////////
         // Send GMP message //
         //////////////////////
 
         // Deposit USDT on Sender contract by triggering teleport fn
-        GmpTestTools.switchNetwork(SEPOLIA_NETWORK, ALICE);
+        GmpTestTools.switchNetwork(SEPOLIA_NETWORK, USER);
         uint256 fee = sender.teleportCost(SHIBUYA_NETWORK, address(receiver), 200);
         console.log("Fee: ", fee);
 
@@ -82,25 +82,25 @@ contract PrototypeTest is Test {
         USDT(usdt).approve(address(sender), 200);
 
         vm.expectEmit(false, true, true, true, address(sender));
-        emit Sender.OutboundTransfer(bytes32(0), ALICE, address(receiver), 200);
+        emit Sender.OutboundTransfer(bytes32(0), USER, address(receiver), 200);
         bytes32 messageID = sender.teleport{value: fee}(address(receiver), 200);
 
         ///////////////////////////////////////////
         // Wait Chronicles Relay the GMP message //
         ///////////////////////////////////////////
 
-        // Now with the `messageID`, Alice can check the message status in the destination gateway contract
+        // Now with the `messageID`, User can check the message status in the destination gateway contract
         // status 0: means the message is pending
         // status 1: means the message was executed successfully
         // status 2: means the message was executed but reverted
-        GmpTestTools.switchNetwork(SHIBUYA_NETWORK, ALICE);
-        console.log("SHIBUYA NETWORK GMP not executed yet Alice should have 0 wUSDT balance -> Alice wUSDT Balance: ", wusdt.balanceOf(ALICE));
+        GmpTestTools.switchNetwork(SHIBUYA_NETWORK, USER);
+        console.log("SHIBUYA NETWORK GMP not executed yet User should have 0 wUSDT balance -> User wUSDT Balance: ", wusdt.balanceOf(USER));
         assertTrue(SHIBUYA_GATEWAY.gmpInfo(messageID).status == GmpStatus.NOT_FOUND, "unexpected message status, expect 'pending'");
 
         // Note: In a live network, the GMP message will be relayed by Chronicle Nodes after a minimum number of confirmations.
         // here we can simulate this behavior by calling `GmpTestTools.relayMessages()`, this will relay all pending messages.
         //vm.expectEmit(true, true, false, true, address(receiver));
-        //emit Receiver.InboundTransfer(messageID, ALICE, address(receiver), 200);
+        //emit Receiver.InboundTransfer(messageID, User, address(receiver), 200);
         GmpTestTools.relayMessages();
 
         // Success! The GMP message was executed!!!
@@ -108,8 +108,8 @@ contract PrototypeTest is Test {
 
         GmpTestTools.switchNetwork(SEPOLIA_NETWORK);
         console.log("\nBalances after bridge...");
-        console.log("SEPOLIA NETWORK -> Alice USDT Balance: ", usdt.balanceOf(ALICE));
-        GmpTestTools.switchNetwork(SHIBUYA_NETWORK, ALICE);
-        console.log("SHIBUYA NETWORK -> Alice wUSDT Balance: ", wusdt.balanceOf(ALICE));
+        console.log("SEPOLIA NETWORK -> User USDT Balance: ", usdt.balanceOf(USER));
+        GmpTestTools.switchNetwork(SHIBUYA_NETWORK, USER);
+        console.log("SHIBUYA NETWORK -> User wUSDT Balance: ", wusdt.balanceOf(USER));
     }
 }
