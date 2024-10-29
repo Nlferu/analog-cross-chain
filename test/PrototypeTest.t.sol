699b40cace0cdda28557ca3f381eb96d684522a6
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-10-29 17:16:58 +0100

    imports path fix

--- test/PrototypeTest.t.sol
+++ test/PrototypeTest.t.sol
@@ -2,10 +2,10 @@
 pragma solidity ^0.8.25;
 
 import {Test, console} from "forge-std/Test.sol";
-import {Sender} from "../src/Sender.sol";
-import {Receiver} from "../src/Receiver.sol";
-import {USDT} from "../src/USDT.sol";
-import {wUSDT} from "../src/wUSDT.sol";
+import {Sender} from "../src/cross-chain-erc20/Sender.sol";
+import {Receiver} from "../src/cross-chain-erc20/Receiver.sol";
+import {USDT} from "../src/cross-chain-erc20/USDT.sol";
+import {wUSDT} from "../src/cross-chain-erc20/wUSDT.sol";
 import {GmpTestTools} from "@analog-gmp-testing/GmpTestTools.sol";
 import {Gateway} from "@analog-gmp/Gateway.sol";
 import {GmpMessage, GmpStatus, GmpSender, PrimitiveUtils} from "@analog-gmp/Primitives.sol";
