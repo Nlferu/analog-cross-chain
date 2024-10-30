933f509ed0aba35b86d20d00eab4b51617f20c88
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-10-30 17:10:55 +0100

    test adjusted for transfer

--- test/CrossChainTest.t.sol
+++ test/CrossChainTest.t.sol
@@ -14,24 +14,25 @@ contract CrossChainTest is Test {
     SourceNFT source;
 
     address private OWNER = makeAddr("Owner");
+    address private USER = makeAddr("User");
 
     function setUp() public {
+        vm.prank(OWNER);
         source = new SourceNFT("Source", "SRC", "http", OWNER);
 
         deal(OWNER, 100 ether);
     }
 
-    function test_deploy() public {
-        new SourceNFT("Source", "SRC", "http", OWNER);
-    }
-
     function test_lockTokens() public {
+        vm.prank(OWNER);
+        source.safeBatchMint(USER, 10);
+
         uint[] memory tokens = new uint[](3);
         tokens[0] = 1;
         tokens[1] = 5;
         tokens[2] = 7;
 
-        assertEq(source.areTokensLocked(tokens), false);
+        assertEq(source.areTokensUnlocked(tokens), true);
 
         uint[] memory lockTokens = new uint[](2);
         lockTokens[0] = 1;
@@ -39,16 +40,21 @@ contract CrossChainTest is Test {
 
         source.lockTokens(lockTokens);
 
-        assertEq(source.areTokensLocked(lockTokens), true);
-        assertEq(source.areTokensLocked(tokens), false);
+        assertEq(source.areTokensUnlocked(lockTokens), false);
+        assertEq(source.areTokensUnlocked(tokens), false);
 
         uint[] memory unlocked = new uint[](1);
         unlocked[0] = 7;
 
         source.unlockTokens(unlocked);
 
-        assertEq(source.areTokensLocked(unlocked), false);
-        assertEq(source.areTokensLocked(lockTokens), false);
-        assertEq(source.areTokensLocked(tokens), false);
+        assertEq(source.areTokensUnlocked(unlocked), true);
+        assertEq(source.areTokensUnlocked(lockTokens), false);
+        assertEq(source.areTokensUnlocked(tokens), false);
+
+        vm.expectRevert(SourceNFT.TokensActiveOnOtherChain.selector);
+        source.safeBatchTransferFrom(USER, OWNER, tokens);
+
+        source.safeBatchTransferFrom(USER, OWNER, unlocked);
     }
 }
