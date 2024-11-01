63d0d7b0fd119d259f04342799cb9879693d5a99
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-11-01 17:39:45 +0100

    initial tests for source crosschain call fn implemented

--- test/CrossChainTest.t.sol
+++ test/CrossChainTest.t.sol
@@ -9,9 +9,11 @@ import {GmpTestTools} from "@analog-gmp-testing/GmpTestTools.sol";
 import {GmpMessage, GmpStatus, GmpSender, PrimitiveUtils} from "@analog-gmp/Primitives.sol";
 
 contract CrossChainTest is Test {
-    SourceNFT source;
+    //SourceNFT source;
+    //DestinationNFT dest;
 
     address private OWNER = makeAddr("Owner");
+    address private DEVIL = makeAddr("Devil");
     address private USER = makeAddr("User");
 
     /// @dev Try adding Aleph Zero EVM network
@@ -23,49 +25,86 @@ contract CrossChainTest is Test {
     Gateway private constant ETHEREUM_GATEWAY = Gateway(GmpTestTools.SEPOLIA_GATEWAY);
     uint16 private constant ETHEREUM_NETWORK = GmpTestTools.SEPOLIA_NETWORK_ID;
 
-    function setUp() public {
-        address destination = address(0);
+    function setUp() public {}
 
-        vm.prank(OWNER);
-        source = new SourceNFT("Source", "SRC", "http", OWNER, ETHEREUM_GATEWAY, destination, ETHEREUM_NETWORK);
+    function test_teleportTokens() public {
+        GmpTestTools.setup();
 
-        deal(OWNER, 100 ether);
-    }
+        /// @dev Test if normal .deal on 1 chain only will fail!!!
+        GmpTestTools.deal(OWNER, 100 ether);
+        GmpTestTools.deal(DEVIL, 100 ether);
+        GmpTestTools.deal(USER, 100 ether);
 
-    /// @dev Test to be removed as tested functions will be internal
-    function test_lockTokens() public {
-        vm.prank(OWNER);
+        ///////////////////////////////////////////////////////
+        // Deploy the SourceNFT and DestinationNFT contracts //
+        ///////////////////////////////////////////////////////
+
+        // Pre-compute the contract addresses, because the contracts must know each other addresses.
+        /// @dev Deploying from other addresses to get different contract addresses on both chains
+        SourceNFT source = SourceNFT(vm.computeCreateAddress(OWNER, vm.getNonce(OWNER)));
+        DestinationNFT dest = DestinationNFT(vm.computeCreateAddress(DEVIL, vm.getNonce(DEVIL)));
+
+        GmpTestTools.switchNetwork(ALEPH_NETWORK, OWNER);
+        source = new SourceNFT("Source", "SRC", "http", OWNER, ALEPH_GATEWAY, address(dest), ETHEREUM_NETWORK);
+
+        GmpTestTools.switchNetwork(ETHEREUM_NETWORK, DEVIL);
+        dest = new DestinationNFT("Dest", "DST", "https", DEVIL, ETHEREUM_GATEWAY, address(source), ALEPH_NETWORK);
+
+        console.log("Source: ", address(source));
+        console.log("Dest: ", address(dest));
+
+        ///////////////////////////////
+        // Mint Some Tokens For USER //
+        ///////////////////////////////
+
+        GmpTestTools.switchNetwork(ALEPH_NETWORK, OWNER);
         source.safeBatchMint(USER, 10);
+
+        //////////////////////
+        // Send GMP message //
+        //////////////////////
 
         uint[] memory tokens = new uint[](3);
         tokens[0] = 1;
         tokens[1] = 5;
         tokens[2] = 7;
 
-        assertEq(source.areTokensUnlocked(tokens), true);
+        // Calculating Gateway Fee
+        uint fee = source.transferCost(tokens);
 
-        uint[] memory lockTokens = new uint[](2);
-        lockTokens[0] = 1;
-        lockTokens[1] = 7;
-
-        source.lockTokens(lockTokens);
-
-        assertEq(source.areTokensUnlocked(lockTokens), false);
-        assertEq(source.areTokensUnlocked(tokens), false);
-
-        uint[] memory unlocked = new uint[](1);
-        unlocked[0] = 7;
-
-        source.unlockTokens(unlocked);
-
-        assertEq(source.areTokensUnlocked(unlocked), true);
-        assertEq(source.areTokensUnlocked(lockTokens), false);
-        assertEq(source.areTokensUnlocked(tokens), false);
-
-        vm.expectRevert(SourceNFT.TokensActiveOnOtherChain.selector);
-        source.safeBatchTransferFrom(USER, OWNER, tokens);
-
+        // Switch Caller From OWNER To USER
+        vm.stopPrank();
         vm.prank(USER);
-        source.safeBatchTransferFrom(USER, OWNER, unlocked);
+        // We do 'false' here as we do not know messageID
+        vm.expectEmit(false, true, true, true, address(source));
+        emit SourceNFT.OutboundTokensTransfer(bytes32(0), USER, address(dest), tokens);
+        bytes32 messageID = source.crossChainTokensTransferFrom{value: fee}(tokens);
+    }
+
+    /// @dev Test to be removed as tested functions will be internal
+    function test_lockTokens() public {
+        // vm.prank(OWNER);
+        // source.safeBatchMint(USER, 10);
+        // uint[] memory tokens = new uint[](3);
+        // tokens[0] = 1;
+        // tokens[1] = 5;
+        // tokens[2] = 7;
+        // assertEq(source.areTokensUnlocked(tokens), true);
+        // uint[] memory lockTokens = new uint[](2);
+        // lockTokens[0] = 1;
+        // lockTokens[1] = 7;
+        // source.lockTokens(lockTokens);
+        // assertEq(source.areTokensUnlocked(lockTokens), false);
+        // assertEq(source.areTokensUnlocked(tokens), false);
+        // uint[] memory unlocked = new uint[](1);
+        // unlocked[0] = 7;
+        // source.unlockTokens(unlocked);
+        // assertEq(source.areTokensUnlocked(unlocked), true);
+        // assertEq(source.areTokensUnlocked(lockTokens), false);
+        // assertEq(source.areTokensUnlocked(tokens), false);
+        // vm.expectRevert(SourceNFT.TokensActiveOnOtherChain.selector);
+        // source.safeBatchTransferFrom(USER, OWNER, tokens);
+        // vm.prank(USER);
+        // source.safeBatchTransferFrom(USER, OWNER, unlocked);
     }
 }
