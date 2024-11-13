56ec7561b0acc3f385beda5eb5d0c28135c7f4b0
Patryk Wargacki <wargacki.patryq@gmail.com>
2024-11-13 16:12:02 +0100

    contract refactor, transfer functions from ERC721A overridden and compatible with Analog GMP, unused functions removed, contract structure cleared

--- src/cross-chain-nft/DestinationNFT.sol
+++ src/cross-chain-nft/DestinationNFT.sol
@@ -49,30 +49,56 @@ contract DestinationNFT is ERC721A, ERC721AQueryable, Ownable {
         i_sourceNetwork = sourceNetwork;
     }
 
-    /// @notice Leads to Metadata, which is unique for each token
-    function _baseURI() internal view override returns (string memory) {
-        return baseURI;
-    }
-
-    /// @dev Prevents tokenURI from adding tokenId to URI as it should be the same for all tokens
-    function tokenURI(uint256 tokenId) public view virtual override(ERC721A, IERC721A) returns (string memory) {
-        if (!_exists(tokenId)) _revert(URIQueryForNonexistentToken.selector);
-
-        return _baseURI();
-    }
-
-    /// @dev Check if it is used
-    /// @notice Returns total minted tokens amount ignoring performed burns
-    /// @dev Call 'totalSupply()' function for amount corrected by burned tokens amount
-    function totalMinted() external view returns (uint256) {
-        return _totalMinted();
-    }
-
     /// @dev CROSS-CHAIN FUNCTIONS
 
     /// @dev Temporary function to until we solve double relay call underflow/overflow error
     function mint(uint id) external {
         _safeMintSpot(msg.sender, id);
+    }
+
+    /// @dev Include function signature check to make below conditional check
+    function transferCost(address to, uint[] memory tokenIds, bool transfer) external view returns (uint256 cost) {
+        bytes memory message = abi.encode(TeleportData({from: msg.sender, to: to, tokens: tokenIds, transfer: transfer}));
+
+        return i_trustedGateway.estimateMessageCost(i_sourceNetwork, message.length, MSG_GAS_LIMIT);
+    }
+
+    /// @dev Below also overrides 'safeTransferFrom(from,to,tokenId)' and 'safeTransferFrom(from,to,tokenId,_data)'
+    function transferFrom(address from, address to, uint256 tokenId) public payable override(ERC721A, IERC721A) {
+        super.transferFrom(from, to, tokenId);
+
+        uint[] memory tokenIds = new uint[](1);
+        tokenIds[0] = tokenId;
+
+        bytes memory message = abi.encode(TeleportData({from: msg.sender, to: to, tokens: tokenIds, transfer: false}));
+
+        /// @dev Function 'submitMessage()' sends message from chain A to chain B
+        /// @param sourceAddress the target address on the source chain
+        /// @param sourceNetwork the target chain where the contract call will be made
+        /// @param executionGasLimit the gas limit available for the contract call
+        /// @param data message data with no specified format
+        bytes32 messageID = i_trustedGateway.submitMessage{value: msg.value}(i_sourceContract, i_sourceNetwork, MSG_GAS_LIMIT, message);
+
+        emit OutboundOwnershipChange(messageID, msg.sender, to, tokenIds);
+    }
+
+    function safeBtachTransfer(address to, uint256[] memory tokenIds) public payable returns (bytes32 messageID) {
+        for (uint i; i < tokenIds.length; i++) {
+            /// @dev Calling original 'transferFrom()' to avoid double cross-chain request
+            /// @dev We cannot call 'safeTransferFrom(from,to,tokenId)' or 'safeTransferFrom(from,to,tokenId,_data)' here as those are overridden by 'transferFrom()'
+            super.transferFrom(msg.sender, to, tokenIds[i]);
+        }
+
+        bytes memory message = abi.encode(TeleportData({from: msg.sender, to: to, tokens: tokenIds, transfer: false}));
+
+        /// @dev Function 'submitMessage()' sends message from chain A to chain B
+        /// @param sourceAddress the target address on the source chain
+        /// @param sourceNetwork the target chain where the contract call will be made
+        /// @param executionGasLimit the gas limit available for the contract call
+        /// @param data message data with no specified format
+        messageID = i_trustedGateway.submitMessage{value: msg.value}(i_sourceContract, i_sourceNetwork, MSG_GAS_LIMIT, message);
+
+        emit OutboundOwnershipChange(messageID, msg.sender, to, tokenIds);
     }
 
     function crossChainTokensTransfer(uint256[] memory tokenIds) external payable returns (bytes32 messageID) {
@@ -97,50 +123,6 @@ contract DestinationNFT is ERC721A, ERC721AQueryable, Ownable {
         emit OutboundTokensTransfer(messageID, msg.sender, i_sourceContract, tokenIds);
     }
 
-    /// @dev To be refactored
-    function safeTransferFrom(address from, address to, uint256 tokenId) public payable override(ERC721A, IERC721A) {
-        super.safeTransferFrom(from, to, tokenId);
-
-        uint[] memory tokenIds = new uint[](1);
-        tokenIds[0] = tokenId;
-
-        bytes memory message = abi.encode(TeleportData({from: msg.sender, to: to, tokens: tokenIds, transfer: false}));
-
-        /// @dev Function 'submitMessage()' sends message from chain A to chain B
-        /// @param sourceAddress the target address on the source chain
-        /// @param sourceNetwork the target chain where the contract call will be made
-        /// @param executionGasLimit the gas limit available for the contract call
-        /// @param data message data with no specified format
-        bytes32 messageID = i_trustedGateway.submitMessage{value: msg.value}(i_sourceContract, i_sourceNetwork, MSG_GAS_LIMIT, message);
-
-        emit OutboundOwnershipChange(messageID, msg.sender, to, tokenIds);
-    }
-
-    /// @dev Consider change to internal
-    function safeBtachTransfer(address to, uint256[] memory tokenIds) public payable returns (bytes32 messageID) {
-        for (uint i; i < tokenIds.length; i++) {
-            super.safeTransferFrom(msg.sender, to, tokenIds[i]);
-        }
-
-        bytes memory message = abi.encode(TeleportData({from: msg.sender, to: to, tokens: tokenIds, transfer: false}));
-
-        /// @dev Function 'submitMessage()' sends message from chain A to chain B
-        /// @param sourceAddress the target address on the source chain
-        /// @param sourceNetwork the target chain where the contract call will be made
-        /// @param executionGasLimit the gas limit available for the contract call
-        /// @param data message data with no specified format
-        messageID = i_trustedGateway.submitMessage{value: msg.value}(i_sourceContract, i_sourceNetwork, MSG_GAS_LIMIT, message);
-
-        emit OutboundOwnershipChange(messageID, msg.sender, to, tokenIds);
-    }
-
-    /// @dev Include function signature check to make below conditional check
-    function transferCost(address to, uint[] memory tokenIds, bool transfer) external view returns (uint256 cost) {
-        bytes memory message = abi.encode(TeleportData({from: msg.sender, to: to, tokens: tokenIds, transfer: transfer}));
-
-        return i_trustedGateway.estimateMessageCost(i_sourceNetwork, message.length, MSG_GAS_LIMIT);
-    }
-
     function onGmpReceived(bytes32 id, uint128 network, bytes32 sender, bytes calldata data) external payable returns (bytes32) {
         address source = address(uint160(uint256(sender)));
 
@@ -150,7 +132,7 @@ contract DestinationNFT is ERC721A, ERC721AQueryable, Ownable {
 
         TeleportData memory command = abi.decode(data, (TeleportData));
 
-        /// @dev Minting all tokens exactly as they exist on source NFT we avoid need of additional mapping, but we bear additional cost of not using batchMint
+        /// @dev Minting all tokens exactly as they exist on source chain. We avoid need of additional mapping, but we bear additional cost of not using batchMint
         for (uint i; i < command.tokens.length; i++) {
             _safeMintSpot(command.from, command.tokens[i]);
         }
@@ -160,16 +142,30 @@ contract DestinationNFT is ERC721A, ERC721AQueryable, Ownable {
         return id;
     }
 
-    /// @dev REQUIRED FUNCTIONS OVERRIDES
+    /// @dev ERC721A FUNCTIONS OVERRIDES ENABLING CUSTOM TOKENURI AND CROSS-CHAIN TRANSFERS
 
     /// @dev Enables use of `_safeMintSpot()` function
     function _sequentialUpTo() internal pure override returns (uint256) {
         return 1;
     }
 
+    /// @notice Leads to Metadata, which is unique for each token
+    function _baseURI() internal view override returns (string memory) {
+        return baseURI;
+    }
+
+    /// @dev Prevents tokenURI from adding tokenId to URI as it should be the same for all tokens
+    function tokenURI(uint256 tokenId) public view override(ERC721A, IERC721A) returns (string memory) {
+        if (!_exists(tokenId)) _revert(URIQueryForNonexistentToken.selector);
+
+        return _baseURI();
+    }
+
+    /// @dev REQUIRED FUNCTIONS OVERRIDES
+
     /// @notice Override ERC721A and ERC721AVotes Function
     /// @dev Additionally delegates vote to new token owner
-    function _afterTokenTransfers(address from, address to, uint256 startTokenId, uint256 quantity) internal virtual override(ERC721A) {
+    function _afterTokenTransfers(address from, address to, uint256 startTokenId, uint256 quantity) internal override(ERC721A) {
         super._afterTokenTransfers(from, to, startTokenId, quantity);
     }
 }
